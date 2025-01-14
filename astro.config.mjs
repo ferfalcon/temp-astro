e2d7d3d12e6f8f13bd2fc4dd92cb4bc946ec3315
Fer <fer.falcon.dg@gmail.com>
2025-01-14 11:45:18 -0300

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://ferfalcon.github.io/temp-astro/',
+	base: '/temp-astro/',
+});
